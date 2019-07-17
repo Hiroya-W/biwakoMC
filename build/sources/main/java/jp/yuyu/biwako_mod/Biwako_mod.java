53eaed7e3526099469199a5380bd904243bfe992
Hiroya-W <e8239@g.maizuru-ct.ac.jp>
2019-07-17 21:09:17 +0900

    [add] 琵琶湖の水を追加
    ツール系の素材として利用される琵琶湖の水を追加した

--- build/sources/main/java/jp/yuyu/biwako_mod/Biwako_mod.java
+++ build/sources/main/java/jp/yuyu/biwako_mod/Biwako_mod.java
@@ -1,13 +1,18 @@
 package jp.yuyu.biwako_mod;
 
 import net.minecraft.block.Block;
+import net.minecraft.client.renderer.block.model.ModelResourceLocation;
+import net.minecraft.creativetab.CreativeTabs;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemSword;
+import net.minecraftforge.client.event.ModelRegistryEvent;
+import net.minecraftforge.client.model.ModelLoader;
 import net.minecraftforge.event.RegistryEvent;
-import net.minecraftforge.fml.common.event.FMLInitializationEvent;
-import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
+import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
-import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
 import net.minecraftforge.fml.common.registry.GameRegistry;
 
 @Mod(
@@ -18,96 +23,74 @@ import net.minecraftforge.fml.common.registry.GameRegistry;
 public class Biwako_mod {
 
     public static final String MOD_ID = "biwako_mod";
-    public static final String MOD_NAME = "Biwako_mod";
-    public static final String VERSION = "2019.1-1.2.23";
+    public static final String MOD_NAME = "Biwako Mod";
+    public static final String VERSION = "1.0.1";
 
-    /**
-     * This is the instance of your mod as created by Forge. It will never be null.
-     */
     @Mod.Instance(MOD_ID)
     public static Biwako_mod INSTANCE;
 
-    /**
-     * This is the first initialization event. Register tile entities here.
-     * The registry events below will have fired prior to entry to this method.
-     */
     @Mod.EventHandler
     public void preinit(FMLPreInitializationEvent event) {
 
     }
 
-    /**
-     * This is the second initialization event. Register custom recipes
-     */
     @Mod.EventHandler
     public void init(FMLInitializationEvent event) {
 
     }
 
-    /**
-     * This is the final initialization event. Register actions from other mods here
-     */
     @Mod.EventHandler
     public void postinit(FMLPostInitializationEvent event) {
 
     }
 
-    /**
-     * Forge will automatically look up and bind blocks to the fields in this class
-     * based on their registry name.
-     */
     @GameRegistry.ObjectHolder(MOD_ID)
     public static class Blocks {
-      /*
-          public static final MySpecialBlock mySpecialBlock = null; // placeholder for special block below
-      */
+
     }
 
     /**
-     * Forge will automatically look up and bind items to the fields in this class
-     * based on their registry name.
+     * アイテム保持用内部クラス
      */
     @GameRegistry.ObjectHolder(MOD_ID)
     public static class Items {
-      /*
-          public static final ItemBlock mySpecialBlock = null; // itemblock for the block above
-          public static final MySpecialItem mySpecialItem = null; // placeholder for special item below
-      */
+        public static final Item biwakoBurger = null;       /** 琵琶湖バーガー */
+        public static final Item biwakoWater = null;        /** 琵琶湖の水 */
+        public static final Item biwakoSword = null;        /** 琵琶湖ソード , 滋賀剣*/
     }
 
     /**
-     * This is a special class that listens to registry events, to allow creation of mod blocks and items at the proper time.
+     * アイテム登録用イベント
      */
     @Mod.EventBusSubscriber
     public static class ObjectRegistryHandler {
-        /**
-         * Listen for the register event for creating custom items
-         */
         @SubscribeEvent
         public static void addItems(RegistryEvent.Register<Item> event) {
-           /*
-             event.getRegistry().register(new ItemBlock(Blocks.myBlock).setRegistryName(MOD_ID, "myBlock"));
-             event.getRegistry().register(new MySpecialItem().setRegistryName(MOD_ID, "mySpecialItem"));
-            */
+            event.getRegistry().registerAll(
+                new Item()
+                        .setRegistryName(MOD_ID, "biwakoburger")
+                        .setCreativeTab(CreativeTabs.FOOD)
+                        .setTranslationKey("biwakoburger"),
+                new Item()
+                        .setRegistryName(MOD_ID,"biwakowater")
+                        .setCreativeTab(CreativeTabs.MATERIALS)
+                        .setTranslationKey("biwakowater")
+            );
+        }
+
+        @SubscribeEvent
+        public static void addBlocks(RegistryEvent.Register<Block> event) {
+
         }
 
         /**
-         * Listen for the register event for creating custom blocks
+         * モデル登録用イベント
          */
         @SubscribeEvent
-        public static void addBlocks(RegistryEvent.Register<Block> event) {
-           /*
-             event.getRegistry().register(new MySpecialBlock().setRegistryName(MOD_ID, "mySpecialBlock"));
-            */
+        public static void registerModels(ModelRegistryEvent event) {
+            ModelLoader.setCustomModelResourceLocation(Items.biwakoBurger, 0, new ModelResourceLocation(Items.biwakoBurger.getRegistryName(), "inventory"));
+            ModelLoader.setCustomModelResourceLocation(Items.biwakoWater, 0, new ModelResourceLocation(Items.biwakoWater.getRegistryName(), "inventory"));
         }
     }
-    /* EXAMPLE ITEM AND BLOCK - you probably want these in separate files
-    public static class MySpecialItem extends Item {
 
-    }
-
-    public static class MySpecialBlock extends Block {
-
-    }
-    */
 }
