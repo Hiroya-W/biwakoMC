54312c1769ac1ce5a2e35f922de3d9b665c10fbb
Hiroya-W <e8239@g.maizuru-ct.ac.jp>
2019-08-07 22:16:36 +0900

    [add] 琵琶湖の県を追加
    琵琶湖の県を追加した。アイテムだけ追加して、レシピはまだ。

---
+++ src/main/java/jp/yuyu/biwako_mod/AddSword.java
@@ -0,0 +1,9 @@
+package jp.yuyu.biwako_mod;
+
+import net.minecraft.item.ItemSword;
+
+public class AddSword extends ItemSword {
+    public AddSword(ToolMaterial material){
+        super(material);
+    }
+}
